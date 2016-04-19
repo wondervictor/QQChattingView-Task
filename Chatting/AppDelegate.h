d57e1229f9a252f283035ede69cb40455e4b3a3b
VicChan <victorchanchina@gmail.com>
2016-04-19 12:26:40 +0800

    Initial Commit

---
+++ Chatting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chatting
+//
+//  Created by VicChan on 4/19/16.
+//  Copyright © 2016 VicChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chatting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chatting
+//
+//  Created by VicChan on 4/19/16.
+//  Copyright © 2016 VicChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
