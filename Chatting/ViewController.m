bff33d9f0eb71d46f5d617cf7ea321b9cc9c1865
VicChan <victorchanchina@gmail.com>
2016-04-27 17:39:44 +0800

    修复了一下手势引起的bug

--- Chatting/ViewController.m
+++ Chatting/ViewController.m
@@ -85,23 +85,7 @@ static NSString *const cellIdentifierFour = @"MessageCellFour";
     
     
     
-    
-    /*
-    
-    [self.messageListArray addObject:@"主要问题在于TabelView第一次进入和每次reloadData时，都会先GetTabelCount，然后对每一项GetCellHeight，然后对显示的的每一项去GetCellData于是，我找到的几乎所有的cell高度自动适应内容的处理办法，其核心思路都是在GetCellHeight时候进行预先计算。这个也能理解。但是大多数是基于"];
-    [self.messageListArray addObject:@"[label.text sizeWithFont:label.font]，这要求对cell的style指定要精确，而且重复工作，计算复杂而结果不精确。我想找到一种直接根据cell的子视图，填充上data就能自动生成高度的方法。我看到过这样的代码："];
-    [self.messageListArray addObject:@"注意语句1和2，1是不可行的，原因如前所提，会造成getcount和getheight间的递归调用直到栈溢出。但是2可行，我没有想明白是怎么回事，我原以为2和1一样都可能栈溢出？问题：1／语句2为什么不会出错，和语句1有什么区别"];
-    [self.messageListArray addObject:@"  2／语句3原先是为了自动取得赋值后的cell高度，但是却失败了。我还尝试过其它方法，比如访问frame／bounds等等，也不行。有没有什么好的预先自动计算高度的方法？不使用语句2也行的。3／如果成功预计算出了不同cell的高度，那么在GetCellData中，对于reuse和createNewCell是否需要不同的IdentifyId，即使style相同。对于不是自己create的而是从nib中读取cell的这种，又是怎么设置高度不同的的IdentifyId呢？IdentifyId到底是根据什么而变化的？"];
-    [self.messageListArray addObject:@"wferwg"];
-    [self.messageListArray addObject:@"dd"];
-    [self.messageListArray addObject:@"掐指算下来做iOS开发也是有两年多的时间了，然后今天一个超级常用的控件让我颜面大跌，于是我准备把自己的丢人行径公之于众。如果您看到我这篇文章时和我一样，也是刚刚知道这项功能，那么您就当收获了一个。。。（其实不算什么），如果您早就知道了 ，那您可以无限的嘲笑我了首先交代一下事情的前因 就是 我有个位置要显示图片，但是美工的图片和我的位置无论大小还是比例 都是不对的，但是同事说这样就可以了 自己处理一下就行 这地方要求不高，但是我就不知道怎么处理才行，于是乎还是请教了同事 最后发现 哎 这东西真的是早就该知道的呀。　我发现自己很是喜欢说废话 说问题之前先介绍一下UIView的contente属性我这次用的时候主要是UIImageView用来显示图片的  这个属性的默认是 UIViewContentModeScaleToFill 这个就是常看到的图片会变形填满；主要说一下这两个属性"];
-    [self.messageListArray addObject:@"看来这样我就只好用［string SizeWithFont font］来慢慢算"];
-    [self.messageListArray addObject:@" 哎 这东西真的是早就该知道的呀。　我发现自己很是喜欢说废话 说问题之前先介绍一下UIView的contente属性我这次用的时候主要是UIImageView用来显示图片的  这个属性的默认是 UIViewContentModeScaleToFill 这个就是常看到的图片会变形填满；主要说一下这两个属性"];
-    [self.messageListArray addObject:@" 哎 这东西真的是早就该知道的呀。　我发现自己很是喜欢说废话 说问题之前先介绍一下UIView的contente属性我这次用的时候主要是UIImageView用来显示图片的  这个属性的默认是 UIViewContentModeScaleToFill 这个就是常看到的图片会变形填满；主要说一下这两个属性"];
-     
-     */
-    
-    [self.messageListArray addObject:@{@"message":@"哎 这东西真的是早就该知道的呀。　我发现自己很是喜欢说废话 说问题之前先介绍一下UIView的contente属性我这次用的时候主要是UIImageView用来显示图片的  这个属性的默认是 UIViewContentModeScaleToFill 这个就是常看到的图片会变形填满；主要说一下这两个属性",@"index":@"0"}];
+       [self.messageListArray addObject:@{@"message":@"哎 这东西真的是早就该知道的呀。　我发现自己很是喜欢说废话 说问题之前先介绍一下UIView的contente属性我这次用的时候主要是UIImageView用来显示图片的  这个属性的默认是 UIViewContentModeScaleToFill 这个就是常看到的图片会变形填满；主要说一下这两个属性",@"index":@"0"}];
     
     [self.messageListArray addObject:@{@"message":@"掐指算下来做iOS开发也是有两年多的时间了，然后今天一个超级常用的控件让我颜面大跌，于是我准备把自己的丢人行径公之于众。如果您看到我这篇文章时和我一样，也是刚刚知道这项功能，那么您就当收获了一个。。。（其实不算什么），如果您早就知道了 ，那您可以无限的嘲笑我了首先交代一下事情的前因 就是 我有个位置要显示图片，但是美工的图片和我的位置无论大小还是比例 都是不对的，但是同事说这样就可以了 自己处理一下就行 这地方要求不高，但是我就不知道怎么处理才行，于是乎还是请教了同事 最后发现 哎 这东西真的是早就该知道的呀。　我发现自己很是喜欢说废话 说问题之前先介绍一下UIView的contente属性我这次用的时候主要是UIImageView用来显示图片的  这个属性的默认是 UIViewContentModeScaleToFill 这个就是常看到的图片会变形填满；主要说一下这两个属性",@"index":@"1"}];
     [self.messageListArray addObject:@{@"message":@"主要问题在于TabelView第一次进入和每次reloadData时，都会先GetTabelCount，然后对每一项GetCellHeight，然后对显示的的每一项去GetCellData于是，我找到的几乎所有的cell高度自动适应内容的处理办法，其核心思路都是在GetCellHeight时候进行预先计算。这个也能理解。但是大多数是基于",@"index":@"2"}];
@@ -128,7 +112,7 @@ static NSString *const cellIdentifierFour = @"MessageCellFour";
     self.tapKeyBoardGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(shutKeyBoard:)];
     self.tapKeyBoardGesture.numberOfTapsRequired = 1;
     self.tapKeyBoardGesture.numberOfTouchesRequired = 1;
-    [self.messageTableView addGestureRecognizer:self.tapKeyBoardGesture ];
+    //[self.messageTableView addGestureRecognizer:self.tapKeyBoardGesture ];
     
     
     self.pickerController = [[UIImagePickerController alloc]init];
@@ -363,7 +347,6 @@ static NSString *const cellIdentifierFour = @"MessageCellFour";
         self.contentView.frame = rect;
         
     } completion:^(BOOL finished) {
-        [self.messageTableView removeGestureRecognizer:self.tapKeyBoardGesture];
 
             NSIndexPath *indexPath = [NSIndexPath indexPathForItem:[self.messageListArray count]-1 inSection:0];
             [self.messageTableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionBottom animated:NO];
@@ -375,6 +358,8 @@ static NSString *const cellIdentifierFour = @"MessageCellFour";
     NSLog(@"saDFSGDHFJK");
     if (self.keyboardShow == YES) {
         [self.messageText resignFirstResponder];
+        [self.messageTableView removeGestureRecognizer:self.tapKeyBoardGesture];
+
     }
     else if (self.menuShow == YES) {
         CGRect rect = self.contentView.frame;
@@ -412,7 +397,6 @@ static NSString *const cellIdentifierFour = @"MessageCellFour";
     
     [self.messageTableView addGestureRecognizer:self.tapKeyBoardGesture];
     NSLog(@"add pic");
-    [self.messageTableView addGestureRecognizer:self.tapKeyBoardGesture];
     CGRect rect = self.contentView.frame;
     if (self.menuShow == YES) {
         return;
